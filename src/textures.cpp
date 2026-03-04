e9a5cf7e6c46762f5a1cadf309c1ea3c7948a5fa
NatTheBeast <nataniel.avisar@gmail.com>
2026-03-04 18:28:28 -0500

    done Todos in textures.cpp

--- src/textures.cpp
+++ src/textures.cpp
@@ -1,8 +1,6 @@
 #include "textures.hpp"
-
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
-
 #include <iostream>
 
 Texture2D::Texture2D()
@@ -11,45 +9,83 @@ Texture2D::Texture2D()
 
 }
 
+static GLenum channelsFormat(int nChannels) {
+    
+    switch (nChannels) {
+    case 1: return GL_RED;
+    case 2: return GL_RG;
+    case 3: return GL_RGB;
+    case 4: return GL_RGBA;
+    default: return GL_RGB;
+    }
+}
+
 void Texture2D::load(const char* path)
 {
     int width, height, nChannels;
     stbi_set_flip_vertically_on_load(true);
     glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
     unsigned char* data = stbi_load(path, &width, &height, &nChannels, 0);
-    if (data == NULL)
+    if (data == NULL) {
         std::cout << "Error loading texture \"" << path << "\": " << stbi_failure_reason() << std::endl;
+        return;
+    }
 
     // TODO: Chargement de la texture en mémoire graphique.
     //       Attention au format des pixels de l'image!
-    //       Toutes les variables devraient être utilisées (width, height, nChannels, data).
-    
+    //       Toutes les variables devraient être utilisées (width, height, nChannels, data).    
+    if (m_id == 0) {
+        glGenTextures(1, &m_id);
+    }
+
+    glBindTexture(GL_TEXTURE_2D, m_id);
+    const GLenum format = channelsFormat(nChannels);
+    glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+    glBindTexture(GL_TEXTURE_2D, 0);
+
     stbi_image_free(data);
 }
 
 Texture2D::~Texture2D()
 {
     // TODO: Libérer les ressources allouées.
+    if (m_id != 0) {
+        glDeleteTextures(1, &m_id);
+    }
 }
 
 void Texture2D::setFiltering(GLenum filteringMode)
 {
     // TODO: Configurer le filtre min et le mag avec le mode en paramètre.
+    glBindTexture(GL_TEXTURE_2D, m_id);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, filteringMode);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, filteringMode);
+    glBindTexture(GL_TEXTURE_2D, 0);
 }
 
 void Texture2D::setWrap(GLenum wrapMode)
 {
     // TODO: Configurer le wrap S et T avec le mode en paramètre.
+    glBindTexture(GL_TEXTURE_2D, m_id);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, wrapMode);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, wrapMode);
+    glBindTexture(GL_TEXTURE_2D, 0);
 }
 
 void Texture2D::enableMipmap()
 {
     // TODO: Génère le mipmap et configure les paramètres pour l'utiliser.
+    glBindTexture(GL_TEXTURE_2D, m_id);
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glBindTexture(GL_TEXTURE_2D, 0);
 }
 
 void Texture2D::use()
 {
     // TODO: Met la texture active pour être utilisée dans les prochaines commandes de dessins.
+    glBindTexture(GL_TEXTURE_2D, m_id);
 }
 
 //
@@ -81,10 +117,31 @@ void TextureCubeMap::load(const char** pathes)
     //       Faites la configuration des min et mag filtering et du wrap S, T, R directement, ils
     //       ne seront pas modifiés ailleurs.
     
+    if (m_id == 0) {
+        glGenTextures(1, &m_id);
+    }
+
+    glBindTexture(GL_TEXTURE_CUBE_MAP, m_id);
+
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+
     for (unsigned int i = 0; i < 6; i++)
     {
         // TODO
+        if (!datas[i]) continue;
+
+        const GLenum format = channelsFormat(nChannels[i]);
+
+        glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, format, widths[i],heights[i], 0, format, GL_UNSIGNED_BYTE, datas[i]);
     }
+    glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
+
 
     for (unsigned int i = 0; i < 6; i++)
     {
@@ -95,10 +152,14 @@ void TextureCubeMap::load(const char** pathes)
 TextureCubeMap::~TextureCubeMap()
 {
     // TODO: Libérer les ressources allouées.
+    if (m_id != 0) {
+        glDeleteTextures(1, &m_id);
+    }
 }
 
 void TextureCubeMap::use()
 {
     // TODO: Met la texture active pour être utilisée dans les prochaines commandes de dessins.
+    glBindTexture(GL_TEXTURE_CUBE_MAP, m_id);
 }
 
